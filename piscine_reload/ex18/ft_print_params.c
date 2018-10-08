1c0284553a5d5304d6ed08fb0af7a3e12a357a19
Unknown <frrobert@student.42.fr>
2018-10-08 11:36:22 +0200

    ex18 ft_print_params

---
+++ piscine_reload/ex18/ft_print_params.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: towelie <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/08 09:29:53 by towelie           #+#    #+#             */
+/*   Updated: 2018/10/08 10:43:28 by towelie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c,1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+		ft_putchar(*str++);
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
